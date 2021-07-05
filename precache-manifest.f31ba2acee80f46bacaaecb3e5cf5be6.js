b725f4494deea599eba49276090cc140deca12eb
marina-saito <marina-saito@users.noreply.github.com>
2021-07-05 11:25:43 +0000

    deploy: 816a53bfeb8b18e9ac1731ecf683fa49b0064597

---
+++ precache-manifest.f31ba2acee80f46bacaaecb3e5cf5be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658f4b8073b3aed82884f9ef25e4d806",
+    "url": "https://marina-saito.github.io/nestjs-frontend/index.html"
+  },
+  {
+    "revision": "42c2addafd74604a413e",
+    "url": "https://marina-saito.github.io/nestjs-frontend/static/css/main.5e345a88.chunk.css"
+  },
+  {
+    "revision": "ab15277451eab88aee21",
+    "url": "https://marina-saito.github.io/nestjs-frontend/static/js/2.7dc14255.chunk.js"
+  },
+  {
+    "revision": "42c2addafd74604a413e",
+    "url": "https://marina-saito.github.io/nestjs-frontend/static/js/main.4387faef.chunk.js"
+  },
+  {
+    "revision": "e68954f95bf9abd930a9",
+    "url": "https://marina-saito.github.io/nestjs-frontend/static/js/runtime~main.a7c09cd6.js"
+  }
+]);
